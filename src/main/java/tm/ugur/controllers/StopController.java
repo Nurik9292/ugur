53012e225b616c1a27b3e8d34af5dba28a503e51
Nury9292 <nurydavletov@gmail.com>
2024-01-11 14:18:59 +0500

    create Stop controller index

--- src/main/java/tm/ugur/controllers/StopController.java
+++ src/main/java/tm/ugur/controllers/StopController.java
@@ -1,4 +1,25 @@
 package tm.ugur.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import tm.ugur.services.StopService;
+
+@Controller
 public class StopController {
+
+    private final StopService stopService;
+
+    @Autowired
+    public StopController(StopService stopService) {
+        this.stopService = stopService;
+    }
+
+    public String  index(Model model){
+        model.addAttribute("title", "Остановка");
+        model.addAttribute("stops", this.stopService.findAll());
+
+        return "layouts/stops/index";
+    }
+
 }
