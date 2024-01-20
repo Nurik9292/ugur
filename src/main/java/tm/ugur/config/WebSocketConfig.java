2f89563a187a2983a9c7cdf6597a77e1b4b15434
Nury9292 <nurydavletov@gmail.com>
2024-01-20 11:03:24 +0500

    change websocket 2

--- src/main/java/tm/ugur/config/WebSocketConfig.java
+++ src/main/java/tm/ugur/config/WebSocketConfig.java
@@ -16,9 +16,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/websocket-ugur")
-                .setAllowedOrigins("*")
-                .withSockJS();
+        registry.addEndpoint("/websocket-ugur");
+//                .setAllowedOrigins("*")
+//                .withSockJS();
     }
 
     @Override
