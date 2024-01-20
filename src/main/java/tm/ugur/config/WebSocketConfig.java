d11cd44ee86ea3ac2da2deec26d8fda9dca2d5b4
Nury9292 <nurydavletov@gmail.com>
2024-01-20 13:06:52 +0500

    change websocket 4

--- src/main/java/tm/ugur/config/WebSocketConfig.java
+++ src/main/java/tm/ugur/config/WebSocketConfig.java
@@ -3,6 +3,8 @@ package tm.ugur.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -10,8 +12,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableScheduling
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
+public class WebSocketConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
 
 
     @Override
@@ -25,4 +26,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages
+                .simpDestMatchers("/websocket-ugur").permitAll()
+                .anyMessage().permitAll();
+    }
+
 }
