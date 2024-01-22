664890750345497442520bab326f1273ff20a438
Nury9292 <nurydavletov@gmail.com>
2024-01-22 11:07:57 +0500

    change ws securety 2

--- src/main/java/tm/ugur/config/WebSocketSecuretyConfig.java
+++ src/main/java/tm/ugur/config/WebSocketSecuretyConfig.java
@@ -1,10 +1,15 @@
 package tm.ugur.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
 @Configuration
+@EnableWebSocketMessageBroker
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
 public class WebSocketSecuretyConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
 
     @Override
