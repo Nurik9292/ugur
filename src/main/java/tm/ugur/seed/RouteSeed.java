ccec06172ab84cb0c3775b278c51a1a0f9829654
Nury9292 <nurydavletov@gmail.com>
2024-02-05 15:40:12 +0500

    change read file

--- src/main/java/tm/ugur/seed/RouteSeed.java
+++ src/main/java/tm/ugur/seed/RouteSeed.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import tm.ugur.dto.RouteDTO;
 import tm.ugur.services.RouteService;
@@ -26,7 +27,8 @@ public class RouteSeed implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         if(this.routeService.findAll().isEmpty()) {
-            try (FileInputStream fis = new FileInputStream(new File("routes.json"))) {
+            ClassPathResource resource = new ClassPathResource("routes.json");
+            try (FileInputStream fis = new FileInputStream(resource.getFile())) {
                 ObjectMapper mapper = new ObjectMapper();
                 List<RouteDTO> routes = mapper.readValue(fis, new TypeReference<List<RouteDTO>>() {
                 });
