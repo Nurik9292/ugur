935f75dbc0dd5930a1ba48394a69a0c224769939
Nury9292 <nurydavletov@gmail.com>
2024-02-06 08:32:07 +0500

    change seed read file class

--- src/main/java/tm/ugur/seed/StopSeed.java
+++ src/main/java/tm/ugur/seed/StopSeed.java
@@ -4,11 +4,11 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import tm.ugur.dto.StopDTO;
 import tm.ugur.services.StopService;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
@@ -26,7 +26,8 @@ public class StopSeed implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         if (this.stopService.findStopByName("Telekeçiler merkezi (Gündogar tarap)").isEmpty()) {
-            try (FileInputStream fis = new FileInputStream(new File("stop.json"))) {
+            ClassPathResource resource = new ClassPathResource("stop.json");
+            try (FileInputStream fis = new FileInputStream(resource.getFile())) {
                 ObjectMapper mapper = new ObjectMapper();
                 List<StopDTO> stops = mapper.readValue(fis, new TypeReference<List<StopDTO>>() {});
                 stops.forEach(this.stopService::save);
