20cd93ac50cee1aa8b12b8c14a499e6750fc396d
Nury9292 <nurydavletov@gmail.com>
2024-02-08 08:53:55 +0500

    fixed error on bus response

--- src/main/java/tm/ugur/services/data_bus/BusResponseService.java
+++ src/main/java/tm/ugur/services/data_bus/BusResponseService.java
@@ -3,20 +3,16 @@ package tm.ugur.services.data_bus;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.*;
 import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import tm.ugur.job.BusScheduling;
 
 import java.util.Collections;
 
 @Service
 public class BusResponseService {
 
-    private final static Logger logger = LoggerFactory.getLogger(BusScheduling.class);
 
     public JsonNode getData(String url, String user, String password) throws JsonProcessingException{
             ResponseEntity<String> response = this.connection(url, user, password);
