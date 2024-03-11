fa1d6a6d5e6a64aef3b88c2fc83fb6b44f34ad26
Nury9292 <nurydavletov@gmail.com>
2024-03-11 11:51:38 +0500

    create repository place category

---
+++ src/main/java/tm/ugur/repo/PlaceCategoryRepository.java
@@ -0,0 +1,9 @@
+package tm.ugur.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tm.ugur.models.PlaceCategory;
+
+@Repository
+public interface PlaceCategoryRepository extends JpaRepository<PlaceCategory, Long> {
+}
