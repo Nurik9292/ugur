0785c66d95edb3e91019712ca5697f170139c1c4
Nury9292 <nurydavletov@gmail.com>
2024-04-30 10:11:20 +0500

    fixed host

--- src/main/resources/static/js/myfunction.js
+++ src/main/resources/static/js/myfunction.js
@@ -1,4 +1,4 @@
-const host = "http:/95.85.127.25:8083";
+const host = "http://95.85.127.25:8083";
 // const host = "http://192.168.37.61:8083";
 // const host = "http://localhost:8080";
 let sortByStop = "";
